45a50da6ad312e149a995dadbb69b3a6466a56b9
Sunny Bibyan <sunnykumar6121997@gmail.com>
2024-12-04 02:53:03 +0530

    route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../../../auth";
+import { authOptions } from "./auth.config"; // authOptions ko alag file me define karein
 
+// NextAuth handler banayein
 const handler = NextAuth(authOptions);
+
+// HTTP methods ke saath export karein
 export { handler as GET, handler as POST };
